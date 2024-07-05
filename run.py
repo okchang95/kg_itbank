4ffb1bc7356456de64a332b0d63d4df6079b3d3c
changwoo <124979889+okchangwoo@users.noreply.github.com>
2024-07-05 10:31:38 +0900

    update run.py

--- run.py
+++ run.py
@@ -9,38 +9,6 @@ def file_exists(filename):
 
 if __name__ =="__main__":
 
-    # def function(evt):  
-    #     lat_value = document.querySelector("#latValue")
-    #     lng_value = document.querySelector("#lngValue")
-    #     radius_value = document.querySelector("#radiusValue")
-    #     date_value = document.querySelector("#dateValue")
-    #     time_value = document.querySelector("#timeValue")
-    #     result= document.querySelector("#result")
-    #     # 입력값 확인
-    #     # result.innerText = f'{lat_value.value, lng_value.value, radius_value.value, date_value.value.split('-'), time_value.value}'
-        
-    #     ## 입력값
-    #     search_lat = lat_value.value    # 위도
-    #     search_lng = lng_value.value    # 경도
-    #     search_radius = radius_value.value  # 반경
-    #     search_date = date_value.value.split('-')
-    #     search_time = time_value.value
-    #     '''
-    #     '37.57035764261156', <class 'str'>
-    #     '126.99036702755654', <class 'str'> 
-    #     '0.3', <class 'str'>
-    #     ['2024', '07', '31'], <class 'list'>
-    #     '00:45', <class 'str'>
-    #     '''
-
-    #     ## 필터링 진행 함수 
-    #     if float(search_radius) > 10:
-    #         result.innerText = "반경을 10 이하로 입력해주세요."
-    #     else:
-    #         # 필터링
-    #         result.innerText = "=========검색결과==========="
-    #         pass
-
     # INPUTS: 들어올것으로 예상
     filename = 'test'
     result_filename = 'result'
@@ -115,5 +83,35 @@ if __name__ =="__main__":
     else:
         print("4) 데이터를 로드할 수 없습니다 ㅠ ...(4)")
 
+    # web
+    # def function(evt):  
+    #     lat_value = document.querySelector("#latValue")
+    #     lng_value = document.querySelector("#lngValue")
+    #     radius_value = document.querySelector("#radiusValue")
+    #     date_value = document.querySelector("#dateValue")
+    #     time_value = document.querySelector("#timeValue")
+    #     result= document.querySelector("#result")
+    #     # 입력값 확인
+    #     # result.innerText = f'{lat_value.value, lng_value.value, radius_value.value, date_value.value.split('-'), time_value.value}'
+        
+    #     ## 입력값
+    #     search_lat = lat_value.value    # 위도
+    #     search_lng = lng_value.value    # 경도
+    #     search_radius = radius_value.value  # 반경
+    #     search_date = date_value.value.split('-')
+    #     search_time = time_value.value
+    #     '''
+    #     '37.57035764261156', <class 'str'>
+    #     '126.99036702755654', <class 'str'> 
+    #     '0.3', <class 'str'>
+    #     ['2024', '07', '31'], <class 'list'>
+    #     '00:45', <class 'str'>
+    #     '''
 
-
+    #     ## 필터링 진행 함수 
+    #     if float(search_radius) > 10:
+    #         result.innerText = "반경을 10 이하로 입력해주세요."
+    #     else:
+    #         # 필터링
+    #         result.innerText = "=========검색결과==========="
+    #         pass
